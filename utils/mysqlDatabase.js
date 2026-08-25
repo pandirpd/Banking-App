23c07529739d19b3176138f84d738ad8e17a200b
Pandi <pandi.rpd@gmail.com>
2026-08-25 22:02:28 +0800

    Add MySQL database utility

---
+++ utils/mysqlDatabase.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+async function createConnection() {
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
+
+module.exports = { createConnection };
